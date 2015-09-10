f5913d287b3eb1a7d01b7463530c50ca8ead9b1b
ulisesGascon <ulisesgascongonzalez@gmail.com>
2015-09-10 00:06:45 +0000

    Geolocation añadido

---
+++ Ulises/otros/geolocation/app.js
@@ -0,0 +1,85 @@
+(function() {
+
+google.maps.visualRefresh = true; // Refresco Constante
+
+var map;
+function initialize() {
+  
+  // Definimos el entorno
+  var mapOptions = {
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('mapa'),
+      mapOptions);
+  
+  // Arrancamos geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Te pille! :-)'
+      });
+      map.setCenter(pos);
+      
+
+      //datos Primer Arranque
+      muestraDatos(x ,position);
+
+    
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Lanzamos aviso -> Error Navegador
+    handleNoGeolocation(false);// Lanzamos aviso -> Error Servicio
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: El Servicio de Geolocalización esta fallando.';
+  } else {
+    var content = 'Error: Tu navegador no soprota la Geolocalización.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+// Datos (Core)
+
+var x = document.getElementById("datos");
+var a = document.getElementById("recarga");
+
+function muestraDatos(div, position){        
+        x.innerHTML = "Latitud: " + position.coords.latitude + 
+        "<br>Longitud: " + position.coords.longitude +
+        "<br>Precisión: " + position.coords.accuracy +
+        "<br>Altitud: " + position.coords.altitude +
+        "<br>Altitud Precisa: " + position.coords.altitudeAccuracy +
+        "<br>Grados Norte: " + position.coords.heading +
+        "<br>Velocidad m/s: " + position.coords.speed + 
+        "<br>Última Conexión: " + position.timestamp;
+};
+
+
+//datos Actualización
+a.onclick = function() {
+        initialize();
+        return false;
+}
+
+})();
